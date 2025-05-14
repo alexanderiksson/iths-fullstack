7b4566263cee8a830a3beb2c17ef74aaf8ddd742
alexanderiksson <Alexerik69@gmail.com>
2025-05-14 15:07:04 +0200

    Added dependencies

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { Client } from "pg";
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+dotenv.config();
+
+const client = new Client({
+    connectionString: process.env.PGURI,
+});
+
+client.connect();
 
 app.get("/", (req, res) => {
     res.send("Hello World");
