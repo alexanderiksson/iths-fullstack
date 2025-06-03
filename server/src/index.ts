ddd82a080636af76cec7c9bf384c9bc3b4814993
alexanderiksson <Alexerik69@gmail.com>
2025-06-03 23:01:02 +0200

    Added port env variable

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ import postRoutes from "./routes/postRoutes";
 import authRoutes from "./routes/authRoutes";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors({ origin: true, credentials: true }));
