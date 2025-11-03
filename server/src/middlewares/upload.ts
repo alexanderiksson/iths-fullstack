32ba24c4418d1635ab03b3b5247177a1d959c1c6
alexanderiksson <Alexerik69@gmail.com>
2025-11-03 15:45:27 +0100

    Fix

--- server/src/middlewares/upload.ts
+++ server/src/middlewares/upload.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
-    destination: "uploads/",
+    destination: "/tmp/uploads/",
     filename: (_req, file, cb) => {
         cb(null, Date.now() + "-" + file.originalname);
     },
